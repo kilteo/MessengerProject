9840a668a2c4eadf648a607f306e849942b5e0f6
kilteo <tkirill2007@mail.ru>
2026-03-22 22:54:02 +0600

    Еще чуть работа с БД

--- server-python/database.py
+++ server-python/database.py
@@ -22,3 +22,21 @@ def save_messages(chat_id, sender_id, text):
     conn.commit()
     cur.close()
     conn.close()
+
+
+def register_user(login, password, name):
+    conn = psycopg2.connect(dbname='messenger_db', user='postgres', password='12345', host='localhost', port='5432')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users WHERE login = %s', (login,))
+    row = cur.fetchone()
+    reg_status = 0
+    if row is not None:
+        reg_status = False
+    else:
+        cur.execute('INSERT INTO users (login, password_hash, display_name) VALUES (%s, %s, %s)', (login, password, name))
+        reg_status = True
+    conn.commit()
+    cur.close()
+    conn.close()
+    return reg_status
+
