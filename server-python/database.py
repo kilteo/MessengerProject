65f33b5e2cb8b732bee99419e807d9e9476a2ed5
kilteo <tkirill2007@mail.ru>
2026-03-22 22:09:45 +0600

    Логика авторизации и сохранение сообщений в БД

--- server-python/database.py
+++ server-python/database.py
@@ -1,1 +1,24 @@
 import psycopg2
+
+def verify_user(login, password):
+    conn = psycopg2.connect(dbname='messenger_db', user='postgres', password='12345', host='localhost', port='5432')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users WHERE login = %s AND password_hash = %s', (login, password))
+    log_status = 0
+    row = cur.fetchone()
+    if row is not None:
+        log_status = row[0]
+    else:
+        log_status = None
+    cur.close()
+    conn.close()
+    return log_status
+
+
+def save_messages(chat_id, sender_id, text):
+    conn = psycopg2.connect(dbname='messenger_db', user='postgres', password='12345', host='localhost', port='5432')
+    cur = conn.cursor()
+    cur.execute('INSERT INTO messages (chat_id,sender_id, message_text) VALUES (%s, %s, %s)', (chat_id, sender_id, text))
+    conn.commit()
+    cur.close()
+    conn.close()
