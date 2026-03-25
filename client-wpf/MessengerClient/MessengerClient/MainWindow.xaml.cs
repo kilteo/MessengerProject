9021214a7c54acf87858d4a9e98bcb6a89123f54
kilteo <tkirill2007@mail.ru>
2026-03-25 11:03:11 +0600

    Создание окна регистрации и входа, так же переход из одного окна в другое

--- client-wpf/MessengerClient/MessengerClient/MainWindow.xaml.cs
+++ client-wpf/MessengerClient/MessengerClient/MainWindow.xaml.cs
@@ -56,5 +56,57 @@ namespace MessengerClient
                 MessageBox.Show("Ошибка подключения: " + ex.Message);
             }
         }
+
+        private void RegistrationButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            LoginPanel.Visibility = Visibility.Collapsed;
+            RegisterPanel.Visibility = Visibility.Visible;
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (RegPasswordBox.Password == RegPasswordCheckBox.Password)
+            {
+                string message;
+                message = ($"REGISTER|{RegLoginBox.Text}|{RegPasswordBox.Password}|{RegNameBox.Text}");
+                try
+                {
+                    TcpClient client = new TcpClient("127.0.0.1", 10000);
+                    NetworkStream stream = client.GetStream();
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                    byte[] buffer = new byte[1024];
+                    int byteRead = stream.Read(buffer, 0, buffer.Length);
+                    string responce = Encoding.UTF8.GetString(buffer, 0, byteRead);
+                    string[] parts = responce.Split('|');
+                    if (parts[0] == "SUCCESS")
+                    {
+                        MessageBox.Show("Добро пожаловать в систему!");
+                        ErrorText.Text = "";
+                    }
+                    else
+                    {
+                        ErrorText.Text = "Данный логин занят!";
+                        RegPasswordBox.Clear();
+                        RegPasswordCheckBox.Clear();
+                    }
+                    client.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка подключения: " + ex.Message);
+                }
+            }
+            else
+            {
+                RegErrorText.Text = "Пароли не совпадают!";
+            }
+        }
+
+        private void RegLoginButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            LoginPanel.Visibility = Visibility.Visible;
+            RegisterPanel.Visibility = Visibility.Collapsed;
+        }
     }   
 }
